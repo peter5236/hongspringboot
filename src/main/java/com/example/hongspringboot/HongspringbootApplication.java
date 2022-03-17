b32aef276f631141a8f32712065d6b54a5601588
peterko5236 <peterko5236@gmail.com>
2022-03-17 14:58:43 +0900

    first commit

---
+++ src/main/java/com/example/hongspringboot/HongspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hongspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hongspringboot/HongspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
